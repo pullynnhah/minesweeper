668f3006f5c8f77336aa9969f7cf7db86ad7eb61
Paula Rodrigues <paulinhaabro@gmail.com>
2022-08-11 15:32:15 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Minesweeper
+
+Check it out [here](https://minesweeper.paulaabro.com).
+
---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import {BrowserRoutes, Routes, Route} from "react-router-dom";
+export default function App() {
+  return (
+    <BrowserRoutes>
+      <Routes></Routes>
+    </BrowserRoutes>
+  );
+}
